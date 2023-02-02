1a7b44a4e771eeb6eafca1f6a3097c67c0892806
Michal Kowalik <klapaucius.mk@gmail.com>
2023-02-02 21:49:30 +0100

    feat: implement led class

---
+++ include/Led.h
@@ -0,0 +1,23 @@
+class Led {
+  private:
+    byte pin;
+
+  public:
+    Led(byte pin) {
+        this->pin = pin;
+        init();
+    }
+
+    void init() {
+        pinMode(pin, OUTPUT);
+        off();
+    }
+
+    void on() {
+        digitalWrite(pin, HIGH);
+    }
+
+    void off() {
+        digitalWrite(pin, LOW);
+    }
+};
